a66e5704d4e34c522f6d2065a9f7957fc84901ec
Vlada Mamutova <36194644+VladaMamutova@users.noreply.github.com>
2020-09-22 23:12:06 +0300

    feat: replace thymeleaf with mustache

--- src/main/kotlin/ru/vladamamutova/GreetingController.kt
+++ src/main/kotlin/ru/vladamamutova/GreetingController.kt
@@ -2,17 +2,24 @@ package ru.vladamamutova
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class GreetingController {
+    @GetMapping("/")
+    fun main(model: Model): String {
+        model["name"] = "World"
+        return "greeting"
+    }
+
     @GetMapping("/greeting")
     fun greeting(
-    @RequestParam(name="name", required=false, defaultValue="World") name: String,
+    @RequestParam(name="name", required=false, defaultValue="user") name: String,
     model: Model
     ): String {
-        model.addAttribute("name", name)
+        model["name"] = name
         return "greeting"
     }
 }
